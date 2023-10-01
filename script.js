8a93584f98031d97464bbad9ac19f2ef31036534
click-jacking <145872369+click-jacking@users.noreply.github.com>
2023-10-01 19:00:54 +0500

    hmm. added a bit more msgs no yet> card.html,

--- script.js
+++ script.js
@@ -31,37 +31,41 @@ const getSessionStorageInputValue = () => {
 
 const checkJack = () => {
   shareBtn.textContent = 'Share results';
+  // Get the URL from the query parameter.
+  url = new URLSearchParams(window.location.search).get('url');
+  // Check if the current URL has a query parameter
+  const urlRegex = /^(https?:\/\/)?([\w.-]+)(\/[^\s]*)?$/;
 
-  // Get the URL from the query parameter or the input field.
-  let url = new URLSearchParams(window.location.search).get('url') || inpt.value.trim();
-
-  // Use a regular expression to validate the URL format
-  const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
   if (!urlRegex.test(url)) {
     // Alert the user if the URL is not valid
     alert('Invalid URL. Please enter a valid URL.');
     return; // Exit the function early
   }
+  if (url) {
+    // Store the searched value in session storage
+    const inputValue = inpt.value.trim();
+    updateSessionStorage(inputValue);
+    // Remove the query parameter from the URL
+    const currentURL = new URL(window.location.href);
+    currentURL.searchParams.delete('url');
+    history.pushState(null, '', currentURL.toString());
 
-  // Store the searched value in session storage
-  updateSessionStorage(url);
-
-  // Remove the query parameter from the URL (if it exists)
-  const currentURL = new URL(window.location.href);
-  currentURL.searchParams.delete('url');
-  history.pushState(null, '', currentURL.toString());
-
-  // Display the URL in the input field
-  inpt.value = url;
-
+    // Treat the session storage value as the URL
+    url = getSessionStorageInputValue();
+    inpt.value = url;
+  } else {
+    // If the URL parameter is not present, use the value of the `inpt` element.
+    url = inpt.value.trim();
+  }
   if (url !== '') {
-    resultsDiv.classList.remove('none');
-    shareBtn.classList.remove('none');
+
+    resultsDiv.classList.remove('none')
+    shareBtn.classList.remove('none')
     secFrame.innerHTML = '';
     fetchIPAddress(url);
     timeDate();
-    performCspHeaderTest(url);
-    performXFrameOptionsTest(url);
+    performCspHeaderTest(url)
+    performXFrameOptionsTest(url)
     const iframe = secFrame.querySelector('iframe');
     if (iframe) {
       secFrame.innerHTML = `<iframe src="${url}" id="frame" frameborder="0"></iframe>`;
@@ -72,14 +76,11 @@ const checkJack = () => {
     // Add a loading indicator.
     secFrame.classList.add('loading');
     secFrame.classList.add('container-100');
-    document.getElementById('frame').onload = function () {
-      secFrame.classList.remove('loading');
-    }; // before setting 'src'
+    document.getElementById('frame').onload = function() {secFrame.classList.remove('loading') }; // before setting 'src'
   } else {
     alert('Enter a URL');
   }
 };
-
 
 //share functionality
 shareBtn.addEventListener('click', () => {
@@ -149,38 +150,48 @@ const fetchIPAddress = async (url) => {
 // Function to perform the X-Frame-Options test
 const performXFrameOptionsTest = async (testUrl) => {
   try {
-    // Make an HTTP request to the URL to get the HTTP headers
-    const response = await fetch(testUrl, { method: 'HEAD' });
+    // Use the CORS Anywhere proxy URL
+    const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+    // Make an HTTP request to the URL using the proxy
+    const response = await fetch(corsProxyUrl + testUrl, { method: 'HEAD' });
     const headers = response.headers;
-    
-    // Check if the headers object is missing or if the X-Frame-Options header is missing or does not include DENY or SAMEORIGIN
-    if (!headers || !headers.has('X-Frame-Options') || (!headers.get('X-Frame-Options').includes('DENY') && !headers.get('X-Frame-Options').includes('SAMEORIGIN'))) {
-      resultsDivElems.xFrame.innerHTML = `Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">`
+
+    // Check if the headers object is missing or if the X-Frame-Options header is missing
+    if (!headers || !headers.has('X-Frame-Options')) {
+      resultsDivElems.xFrame.innerHTML = 'Header Missing <img src="assets/red (1).png" alt="Header Missing" width="50">';
+    } else if (headers.get('X-Frame-Options').includes('SAMEORIGIN')) {
+      resultsDivElems.xFrame.innerHTML = 'Same origin <img src="assets/green.png" alt="Same origin" width="50">';
     } else {
-      resultsDivElems.xFrame.innerHTML = 'Test Passed <img src="assets/green.png" alt="Test Passed" width="50">'
+      resultsDivElems.xFrame.innerHTML = 'Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">';
     }
   } catch (error) {
     console.error('X-Frame-Options test error:', error);
     // If there's an error during the fetch, treat it as a test failure
-    resultsDivElems.xFrame.innerHTML = `Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">`
+    resultsDivElems.xFrame.innerHTML = 'Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">';
   }
 }
-  // Function to perform the CSP Header (Frame-Ancestors) test
-  const performCspHeaderTest = async (testUrl) => {
-    try {
-      // Make an HTTP request to the URL to get the HTTP headers
-      const response = await fetch(testUrl, { method: 'HEAD' })
-      const headers = response.headers;
-  
-      // Check if the CSP Header value allows framing
-      const cspHeader = headers.get('Content-Security-Policy');
-      if (cspHeader.includes("frame-ancestors 'none'")) {
-        resultsDivElems.Csp.innerHTML= `Test Passed <img src="assets/green.png" alt="Test Passed" width="50">`
-      } else {
-        resultsDivElems.Csp.innerHTML= 'Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">';
-      }
-    } catch (error) {
-      console.error('CSP Header test error:', error);
-      resultsDivElems.Csp.innerHTML= 'Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">';
+
+// Function to perform the CSP Header test
+const performCspHeaderTest = async (testUrl) => {
+  try {
+    // Use the CORS Anywhere proxy URL
+    const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+    // Make an HTTP request to the URL using the proxy
+    const response = await fetch(corsProxyUrl + testUrl, { method: 'HEAD' });
+    const headers = response.headers;
+
+    // Check if the CSP Header value allows framing from the same origin
+    const cspHeader = headers.get('Content-Security-Policy');
+    if (cspHeader.includes("frame-ancestors 'self'")) {
+      resultsDivElems.Csp.innerHTML = `Test Passed <img src="assets/green.png" alt="Test Passed" width="50">`
+    } else {
+      resultsDivElems.Csp.innerHTML = 'Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">';
     }
+  } catch (error) {
+    console.error('CSP Header test error:', error);
+    resultsDivElems.Csp.innerHTML = 'Test Failed <img src="assets/red (1).png" alt="Test Failed" width="50">';
   }
+}
+
